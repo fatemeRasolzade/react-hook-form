40a7e6a575854a0c5e38cb541f807f3f1b2b25b4
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2023-05-27 16:48:41 +0330

    fixed some configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import viteLogo from "/vite.svg";
+import reactLogo from "./assets/react.svg";
+import typeScriptLogo from "./assets/typeScript.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -15,21 +13,17 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://typescript.dev" target="_blank">
+          <img
+            src={typeScriptLogo}
+            className="logo typeScript"
+            alt="TypeScript logo"
+          />
+        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Vite + React + Ts</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
