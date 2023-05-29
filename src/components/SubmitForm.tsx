d15c12eefdaf07f56f0dd94ff0d83d2cff35ba7c
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2023-05-29 17:06:10 +0330

    install devtools and set configuration

--- src/components/SubmitForm.tsx
+++ src/components/SubmitForm.tsx
@@ -1,17 +1,24 @@
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+
 const SubmitForm = () => {
+  const form = useForm();
+  const { register, control } = form;
+
   return (
     <div>
-        <form>
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" name="username" />
+      <form>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" {...register("username")} />
 
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" {...register("password")} />
 
-            <button>Submit</button>
-        </form>
+        <button>Submit</button>
+      </form>
+      <DevTool control={control} />
     </div>
-  )
-}
+  );
+};
 
-export default SubmitForm
+export default SubmitForm;
